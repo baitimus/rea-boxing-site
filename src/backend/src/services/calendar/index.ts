7e233f5147be01172b9ec40a314fdafbbea84109
baitimus <cyrus.mauch@gmx.at>
2025-12-19 18:57:08 +0100

    remove unused PDF services and templates; update Dockerfile with new MongoDB credentials

--- src/backend/src/services/calendar/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Calendar service exports
- */
-
-export * from './calendar.service';
-
-// Re-export from dataMapping
-import { getTeacherName, getSubjectName } from './dataMapping';
-export { getTeacherName, getSubjectName };
