5e15105937678484e9083a5671f872c76b21df41
baitimus <cyrus.mauch@gmx.at>
2025-12-19 18:50:38 +0100

    initial frontend clean up removal of old components no renaming yet

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-
+	
+	{ path: '**', redirectTo: '' }
 ];
--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet],
+  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
